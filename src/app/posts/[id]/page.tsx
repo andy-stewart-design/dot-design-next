73fe5b41a764bd7df13c95ae058fee08b040f208
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-06-17 06:35:12 -0400

    starting to style post layout

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,13 +1,14 @@
 import CustomMDX from "@/components/CustomMDX";
 import { getPostBySlug } from "@/utils/blog";
+import s from "./page.module.css";
 
 export default async function Post({ params }: { params: { id: string } }) {
 	const { id } = params;
 	const post = await getPostBySlug(id);
 
 	return (
-		<div>
+		<article className={s.article}>
 			<CustomMDX source={post.content} />
-		</div>
+		</article>
 	);
 }
