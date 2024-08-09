51943c139f9e218f9b0bd2114aa6e31eeda62a1a
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-08-09 07:30:33 -0400

    testing out static rendering for posts

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,6 +2,8 @@ import CustomMDX from "@/components/CustomMDX";
 import { getPostBySlug } from "@/utils/blog";
 import s from "./page.module.css";
 
+export const dynamic = "force-static";
+
 export default async function Post({ params }: { params: { id: string } }) {
 	const { id } = params;
 	const post = await getPostBySlug(id);
