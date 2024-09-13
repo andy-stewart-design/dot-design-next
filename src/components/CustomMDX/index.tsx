8e32b9a6ba7229890d6fc41de58b1fded386e54f
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-09-13 06:02:11 -0400

    Adding new CSS Grid post

--- src/components/CustomMDX/index.tsx
+++ src/components/CustomMDX/index.tsx
@@ -1,17 +1,13 @@
 import * as SVGPieChartComponents from "@/content/drawing-dynamic-svg-pie-charts";
 import CodeSnippet from "../CodeSnippet";
 import { MDXRemote } from "next-mdx-remote/rsc";
-import dynamic from "next/dynamic";
 import Video from "@/components/Video";
 import { Image } from "@/components/Blog";
-
-const SecondCounter = dynamic(() => import("@/content/two/SecondCounter"));
 
 const components = {
 	pre: CodeSnippet,
 	img: Image,
 	Video,
-	SecondCounter,
 	...SVGPieChartComponents,
 };
 
