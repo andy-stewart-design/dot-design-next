bc003d2dfb5c73ebc504e8069c6381303665aae7
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2025-01-04 10:40:08 -0500

    updating page props

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,13 +1,11 @@
 import CustomMDX from "@/components/CustomMDX";
 import { getPostBySlug } from "@/utils/blog";
 import s from "./page.module.css";
-import { PageProps } from "../../../../.next/types/app/posts/[id]/page";
+import { PageProps } from "@/../.next/types/app/posts/[id]/page";
 
 export const dynamic = "force-static";
 
 export default async function Post({ params }: PageProps) {
-	console.log(typeof params);
-
 	const { id } = await params;
 	const post = await getPostBySlug(id);
 
